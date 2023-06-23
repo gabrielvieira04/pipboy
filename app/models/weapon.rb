fdfd07b198de8420c7d2defa8d116a51caa493c2
Brak3 <gabrielvieira040@outlook.com>
2023-06-23 19:29:07 -0300

    feat: adicionado categoria do item a weapons

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,14 +1,15 @@
 # frozen_string_literal: true
 class Weapon < Item
-  attr_reader :weapon_damage
+  attr_reader :weapon_damage, :item_category
 
-  def initialize(item_name, item_valor, weapon_dmg)
+  def initialize(item_name, item_valor, weapon_dmg, item_category)
     @item_name = item_name
     @item_valor = item_valor
     @weapon_dmg = weapon_dmg
+    @item_category = item_category
   end
 
   def to_s
-    "Nome: #{item_name}, Dano: #{@weapon_dmg}, Valor: #{@item_valor}"
+    "Nome: #{item_name}, Dano: #{@weapon_dmg}, Valor: #{@item_valor}, Categoria: #{item_category}"
   end
 end
