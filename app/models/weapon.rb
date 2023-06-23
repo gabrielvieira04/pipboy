ec8b0013e3dee3d7111c4b6800078bde24d06fdd
Brak3 <gabrielvieira040@outlook.com>
2023-06-23 19:20:09 -0300

    feat: adicionado reader para weapon_damage

--- app/models/weapon.rb
+++ app/models/weapon.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
-
 class Weapon < Item
+  attr_reader :weapon_damage
+
   def initialize(item_name, item_valor, weapon_dmg)
     @item_name = item_name
     @item_valor = item_valor
