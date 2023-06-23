2da3ac191106f6f25433e7000cd0f8c24cde4f28
Brak3 <gabrielvieira040@outlook.com>
2023-06-23 19:28:40 -0300

    feat: mudanças na classe

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,11 @@ class Item
   attr_reader :item_name, :item_category
   attr_accessor :valor
 
-  def initialize(item_name, item_valor)
+  def initialize(item_name, item_valor, item_category = :default)
     @item_name = item_name
     @item_valor = item_valor
-    @item_category = :default
+    @item_category = item_category
   end
-
-
 
   def to_s
     "Nome: #{@item_name},Valor: #{@item_valor}, Categoria: #{@item_category}"
