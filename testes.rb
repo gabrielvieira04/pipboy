cec1946cf6e7ac3dfca82ca2c65cf05db0ce87b6
Brak3 <gabrielvieira040@outlook.com>
2023-06-23 19:29:29 -0300

    teste de criação de um item do tippo arma

--- testes.rb
+++ testes.rb
@@ -1,3 +1,8 @@
 # Área para Testes
-
 require File.expand_path("app/pipboy")
+
+pistol_10mm = Weapon.new "Pistola 10mm", 250, 10, :arma_de_fogo
+
+puts pistol_10mm.to_s
+puts pistol_10mm.valor
+puts pistol_10mm.weapon_damage
