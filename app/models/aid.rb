b671ff0eb273f75775456e0c1c74df81c0c22438
Brak3 <gabrielvieira040@outlook.com>
2023-06-23 19:46:38 -0300

    feat: classe aid, para itens de cura criada

---
+++ app/models/aid.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class Aid < Item
+  attr_reader :aid_cure
+
+  def initialialize(item_name, aid_cure, item_valor, item_category = :aid)
+    @item_name = item_name
+    @aid_cure = aid_cure
+    @item_valor = item_valor
+    @item_category = item_category
+  end
+
+  def to_s
+    "Nome: #{item_name}, Cura: #{aid_cure}, Valor: #{@item_valor}, Categoria: #{item_category}"
+  end
+
+end
